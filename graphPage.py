87e76c1b82760e6ea7e5fe5a4e01db3d51014723
Arjun <avonhatten@gmail.com>
2019-02-12 15:11:40 -0500

    Restore SciPi based code

--- graphPage.py
+++ graphPage.py
@@ -1,18 +1,18 @@
 import tkinter as tk
 from tkinter import *
-#from pandas import DataFrame
-#import matplotlib
-#matplotlib.use("TkAgg")
-#import matplotlib.pyplot as plt
-#from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
+from pandas import DataFrame
+import matplotlib
+matplotlib.use("TkAgg")
+import matplotlib.pyplot as plt
+from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
 import settings
 
 Data2 = {'Year': [1920,1930,1940,1950,1960,1970,1980,1990,2000,2010],
         'Unemployment_Rate': [9.8,12,8,7.2,6.9,7,6.5,6.2,5.5,6.3]
        }
   
-#df2 = DataFrame(Data2,columns=['Year','Unemployment_Rate'])
-#df2 = df2[['Year', 'Unemployment_Rate']].groupby('Year').sum()
+df2 = DataFrame(Data2,columns=['Year','Unemployment_Rate'])
+df2 = df2[['Year', 'Unemployment_Rate']].groupby('Year').sum()
 
 class GraphPage(tk.Frame):
 
@@ -24,12 +24,12 @@ class GraphPage(tk.Frame):
         self.mainLabel.config(font=("Times", 50, 'bold'))
         self.mainLabel.grid(row=0, columnspan=2, padx=20)
 
-        #figure2 = plt.Figure(figsize=(5,4), dpi=100)
-        #ax2 = figure2.add_subplot(111)
-        #line2 = FigureCanvasTkAgg(figure2, self)
-        #line2.get_tk_widget().grid(row=2, columnspan=2, pady=20)
-        #df2.plot(kind='line', legend=True, ax=ax2, color='r',marker='o', fontsize=10)
-        #ax2.set_title('Speed Vs. Time')
+        figure2 = plt.Figure(figsize=(5,4), dpi=100)
+        ax2 = figure2.add_subplot(111)
+        line2 = FigureCanvasTkAgg(figure2, self)
+        line2.get_tk_widget().grid(row=2, columnspan=2, pady=20)
+        df2.plot(kind='line', legend=True, ax=ax2, color='r',marker='o', fontsize=10)
+        ax2.set_title('Speed Vs. Time')
 
         self.progress_label = Label(self, text=settings.languageList[1][settings.language])
         self.progress_label.config(font=("Times", 30))

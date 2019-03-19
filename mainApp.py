c62321f47af46c6448f2572f071b7b9465d4cfdb
avonhatten <avonhatten@gmail.com>
2019-03-18 20:55:14 -0400

    Remove auto-propogate

--- mainApp.py
+++ mainApp.py
@@ -13,10 +13,12 @@ class MainApp(tk.Tk):
         # the container is where we'll stack a bunch of frames
         # on top of each other, then the one we want visible
         # will be raised above the others
-        container = tk.Frame(self)
-        container.pack(side="top", expand=True) #, fill="both", expand=True)
+        container = tk.Frame(self, width=1000, height=1000)
+        container.pack(side="top", expand=False) #, fill="both", expand=True)
+        container.pack_propagate(0)
         container.grid_rowconfigure(0, weight=1)
         container.grid_columnconfigure(1, weight=1)
+        container.grid_propagate(0)
 
         self.frames = {}
         self.frames["StartPage"] = startPage.StartPage(parent=container, controller=self)

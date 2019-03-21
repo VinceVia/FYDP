e0fa9431c8ba51e0877251ffe4e1d8c71afdac37
avonhatten <avonhatten@gmail.com>
2019-03-21 17:55:12 -0400

    Add failure mode feature basics

--- resultByIDDao.py
+++ resultByIDDao.py
@@ -17,6 +17,11 @@ class ResultByIDDao:
 		database.execute("SELECT test_status FROM result_by_id WHERE id=(?)", (test_number,))
 		return [item[0] for item in database.fetchall()]
 
+	def get_failure_mode(test_number):
+		database = dao.Database('fydp')
+		database.execute("SELECT failure_mode FROM result_by_id WHERE id=(?)", (test_number,))
+		return [item[0] for item in database.fetchall()]
+
 	def set_test_status():
 		database = dao.Database('fydp')
 		database.execute("SELECT test_status FROM result_by_id WHERE id=(?)", (settings.test_number,))
@@ -39,5 +44,5 @@ class ResultByIDDao:
 
 	def setNewRow():
 		database = dao.Database('fydp')
-		database.execute("INSERT INTO result_by_id(id, sensor_id, test_status) VALUES (?, ? , ?)", (settings.test_number + 1, None, 0))
+		database.execute("INSERT INTO result_by_id(id, sensor_id, test_status, failure_mode) VALUES (?, ? , ?, ?)", (settings.test_number + 1, None, 0, 0))
 		database.commit()

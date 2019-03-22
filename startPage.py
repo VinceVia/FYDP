4eb8f37928790d446500c7303927fd1ecfa8a7b8
avonhatten <avonhatten@gmail.com>
2019-03-22 10:20:14 -0400

    UI

--- startPage.py
+++ startPage.py
@@ -29,16 +29,16 @@ class StartPage(tk.Frame):
         self.results_link.grid(row=2, columnspan=3)
 
         self.start_button = Button(self, borderwidth=5, padx=16, text=settings.languageList[3][settings.language], command=self.start, bg="green")
-        self.start_button.config(font=("Arial", 13, 'bold'))
-        self.start_button.grid(sticky=W, row=3, column=0, padx=5, pady=20, ipady=15)
+        self.start_button.config(font=("Arial", 14, 'bold'))
+        self.start_button.grid(sticky=W, row=3, column=0, padx=5, pady=30, ipady=15)
 
         self.stop_button = Button(self, borderwidth=5, padx=16, text=settings.languageList[4][settings.language], command=self.stop, bg="red")
-        self.stop_button.config(font=("Arial", 13, 'bold'))
-        self.stop_button.grid(sticky=E, row=3, column=1, pady=20, ipady=15)
+        self.stop_button.config(font=("Arial", 14, 'bold'))
+        self.stop_button.grid(sticky=E, row=3, column=1, pady=30, ipady=15)
 
         self.reset_button = Button(self, borderwidth=5, padx=16, text=settings.languageList[29][settings.language], command=self.reset, bg="red")
-        self.reset_button.config(font=("Arial", 13, 'bold'))
-        self.reset_button.grid(sticky=W, row=3, column=2, pady=20, padx=5, ipady=15)
+        self.reset_button.config(font=("Arial", 14, 'bold'))
+        self.reset_button.grid(sticky=W, row=3, column=2, pady=30, padx=5, ipady=15)
 
         if(settings.test_number > 1):
             self.previous_results = Button(self, text=settings.languageList[5][settings.language], fg="blue", relief="flat", command=lambda: controller.show_frame("PreviousGraphPage"))

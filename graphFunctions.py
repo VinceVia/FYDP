ef9d91f85eeaef08f7fa792747577f897c1b45f1
avonhatten <avonhatten@gmail.com>
2019-03-22 10:43:23 -0400

    UI

--- graphFunctions.py
+++ graphFunctions.py
@@ -30,7 +30,7 @@ def plotGraph(self, isPrevious):
             }
     df = DataFrame(Data)
 
-    figure = plt.Figure(figsize=(12,4.5), dpi=150)
+    figure = plt.Figure(figsize=(8,4.5), dpi=60)
     ax = figure.add_subplot(111)
     ax.set_title(settings.languageList[10][settings.language], fontweight="bold", fontsize=16)
     ax.set_xlabel(settings.languageList[12][settings.language])

72bc68016d83cdbcc8a8d02e8f14cb75e60f7d69
VinceVia <50960089+VinceVia@users.noreply.github.com>
2019-06-06 13:56:07 -0400

    Update motorTest1.py

--- motorTest1.py
+++ motorTest1.py
@@ -8,19 +8,29 @@ motorFunctions.connectVFD()
 #TODO: may need to change decelerationTime settings to accelerationTime
 print("Starting Test 1: Run-In, Operating Temperature, and Vibration")
 print("Test 1 - Clockwise direction")
+motorFunctions.setAccelerationTime(15) #1.5s
+motorFunctions.setSpeed(60) #60rpm target speed
+motorFunctions.startMotorForward()
+time.sleep(1.5) #accelerate for 9s
+time.sleep(3) #maintain speed for 10min TODO: change value to 600, add sensors
 motorFunctions.setAccelerationTime(90) #9s
 motorFunctions.setSpeed(900) #900rpm target speed
-motorFunctions.startMotorForward()
 time.sleep(9) #accelerate for 9s
-time.sleep(20) #maintain speed for 10min TODO: change value to 600, add sensors
-motorFunctions.setDecelerationTime(360) #36s
+time.sleep(20) #(Need change)maintain speed for 10min TODO: change value to 600, add sensors
+motorFunctions.setDecelerationTime(225) #22.5s
 motorFunctions.stopMotor() #TODO: check if this uses the right deceleration rate, change to startMotorForwards() with target speed 0 if not
-time.sleep(36) #decelerate for 36s 
+time.sleep(22.5) #decelerate for 36s 
 print("Test 1 - Anticlockwise direction")
 #motorFunctions.setAccelerationTime(900) #9s acceleration time - should be already set from before
+motorFunctions.setAccelerationTime(15) #1.5s
+motorFunctions.setSpeed(60) #60rpm target speed
 motorFunctions.startMotorReverse()
+time.sleep(1.5) #accelerate for 9s
+time.sleep(3) #maintain speed for 10min TODO: change value to 600, add sensors
+motorFunctions.setAccelerationTime(90) #9s
+motorFunctions.setSpeed(900) #900rpm target speed
 time.sleep(9) #accelerate for 9s
 time.sleep(20) #maintain speed for 10min TODO: change to 600, add sensors
 motorFunctions.stopMotor() #TODO: check if this uses the right deceleration rate, change to startMotorBackwards() with target speed 0 if not
-time.sleep(36) #decelerate for 36s 
+time.sleep(22.5) #decelerate for 36s 
 print("Test 1 finished")
